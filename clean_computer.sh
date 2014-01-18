30ee015315aac2abfe1ecf5f39ce162e10df353a
Jon Morehouse <morehousej09@gmail.com>
2014-01-18 13:36:29 -0800

    Update clean script

---
+++ clean_computer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# remove certain files from folders across machine
+files=(
+	
+	.DS_Store
+	.pyc
+)
+
+folders=(
+
+	$HOME/Documents
+	$HOME/Desktop
+	$HOME/Movies
+	$HOME/Pictures
+
+)
+
+for folder in ${folders[@]}
+do
+	for file in ${files[@]}
+	do
+		
+		find $folder -type f -name "*$file" | xargs -I '{}' mv "{}" $HOME/.Trash
+	done
+done
