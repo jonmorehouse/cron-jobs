b2f1ce5985f98eca35b0cc3c485066f852d9f3cd
Jon Morehouse <morehousej09@gmail.com>
2014-01-19 12:15:55 -0800

    Update brew output and fix broken health script

--- health.sh
+++ health.sh
@@ -1,12 +1,14 @@
 #!/usr/local/bin/zsh
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # bootstrap environment so we can grab our brew
 . $HOME/.zshrc
 . $HOME/.ssh/environment 
 
+# set up current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # run brew doctor once per day and ensure that all is working eelle 
-output=`$brew doctor`
+output=`brew doctor 2>&1`
 brew_doctor_output=`cat $DIR/config/expected_brew_doctor_output`
 
 # verify the output to our desktop flag 
@@ -15,13 +17,18 @@ then
 	echo $output > $HOME/Desktop/unhealthy
 
 else
-	output=`$brew update`
+	# system is healthy
+	echo "healthy" >> $HOME/Desktop/healthy
+
+	# now lets output the update output to our machines as needed
+	output=`brew update 2>&1`
+
+	# grab the expected output
 	expected_output=`cat $DIR/config/expected_brew_update_output`
 
 	if [[ "$output" != "$expected_output" ]]
 	then
-		echo $output > $HOME/Desktop/log
+		echo $output >> $HOME/Desktop/log
 	fi
 fi
 
-echo "HEALTHY" >> $HOME/Desktop/healthy
